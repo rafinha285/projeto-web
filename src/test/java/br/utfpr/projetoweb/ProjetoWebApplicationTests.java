2e95732848295a5efdcdada7392c97b5dbb55523
rafinha285 <rafael.schrott02@gmail.com>
2026-06-11 14:04:38 -0300

    initial commit

---
+++ src/test/java/br/utfpr/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
